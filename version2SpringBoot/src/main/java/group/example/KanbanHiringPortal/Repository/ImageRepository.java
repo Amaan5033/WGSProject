33c2cd5d7212da4cdadf8413f506edd3b1ad899d
Amaan5033 <61371025+Amaan5033@users.noreply.github.com>
2022-10-19 00:27:11 +0530

    Add files via upload

---
+++ version2SpringBoot/src/main/java/group/example/KanbanHiringPortal/Repository/ImageRepository.java
@@ -0,0 +1,15 @@
+package group.example.KanbanHiringPortal.Repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import group.example.KanbanHiringPortal.model.Image;
+
+@Repository
+public interface ImageRepository extends JpaRepository<Image,String>{
+	public Optional<Image> findByName(String name);
+}
+	
+
