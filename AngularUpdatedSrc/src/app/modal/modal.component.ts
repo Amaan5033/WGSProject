17d065042c8eef27fd21451c60be48276bb6d032
Amaan5033 <61371025+Amaan5033@users.noreply.github.com>
2022-10-23 18:50:46 +0530

    Add files via upload

---
+++ AngularUpdatedSrc/src/app/modal/modal.component.ts
@@ -0,0 +1,88 @@
+import { Component, OnInit } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+import { KanbanServiceService } from '../kanban-service.service';
+import {HttpClient,HttpEventType} from '@angular/common/http';
+import { Candidate } from '../candidate';
+import { Comment } from '../comment';
+
+
+
+@Component({
+  selector: 'app-modal',
+  templateUrl: './modal.component.html',
+  styleUrls: ['./modal.component.css']
+})
+export class ModalComponent implements OnInit {
+
+  constructor(private _service:KanbanServiceService,
+    public dialogRef:MatDialogRef<ModalComponent>,
+    private _http:HttpClient) { }
+
+  candidateObj:any;
+  commentObj:any;
+
+  commentList:Comment[]=[];
+  // Upload Image Parameter-------------
+
+  uploadImage!:File;
+  dbImage:any;
+  postResponse:any;
+  successResponse!:String;
+  image:any;
+
+
+
+  // --------------------------------------
+  
+
+            
+
+  ngOnInit(): void {
+    
+    console.log("nginIt activated")
+    this._service.giveDatatoModal(this.candidateObj).subscribe(
+      response=>{console.log("i am give data");console.log(response);this.candidateObj=response},
+      error=>{("error occurred")}
+    );
+    this._service.showAllComments(this.commentObj).subscribe(
+      response=>{console.log("i am all comment");console.log("getting all the comments ok");this.commentList=response},
+      error=>{console.log("getting all the comments are not ok")}
+    );
+    console.log("This is from modal component")
+
+  }
+
+
+  closeModal(){
+    this.dialogRef.close();
+  }
+
+  onImageUpload(event:any){
+    this.uploadImage=event.target.files[0];
+  }
+  
+  imageUploadAction(){
+    const imageFormData=new FormData();
+    imageFormData.append('image',this.uploadImage,this.uploadImage.name);
+    this._service.uploadingImage(imageFormData).subscribe(
+      response=>{console.log("response received");this.postResponse=response;
+                                                  this.successResponse=this.postResponse.body.message},
+      error=>{("error occurred");this.successResponse="Image failed to upload"}
+    );
+  }
+
+  viewImage(){
+    this._service.viewingImage(this.image).subscribe(
+      response=>{this.postResponse=response;this.dbImage='data:image/jpeg;base64,'+this.postResponse.image},
+      error=>{}
+    );
+  }
+
+  getComment(value:String){
+    var commentObj=new Comment(this.candidateObj.candidateId,value);
+    this._service.getAllComments(commentObj).subscribe(
+      response=>{console.log("response recieved for comment section");this.commentList=response},
+      error=>{console.log("error occurred in comment section response")}
+    );
+  }
+}
