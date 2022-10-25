042a422dd3ca3255ad0878958fa13b4fb3c22e2c
Amaan5033 <61371025+Amaan5033@users.noreply.github.com>
2022-10-25 17:14:47 +0530

    Add files via upload

---
+++ AngularVerison4/src/app/candidate.ts
@@ -0,0 +1,19 @@
+export class Candidate {
+    candidateId!:number;
+    candidateName:String="";
+    age!:number;
+    totalExperiance!:number;
+    relevantExperiance!:number;
+    expectedLwd:String="";
+    location:String="";
+    skills:String="";
+    technology:String="";
+    maritialStatus:String="";
+    status:number=1;
+    gcm!:String;
+    position!:String;
+    duedate!:String;
+    project!:String;
+    constructor(){}
+
+}
